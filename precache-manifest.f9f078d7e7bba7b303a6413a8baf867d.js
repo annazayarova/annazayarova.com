8bf06ea9904ff602552b336f08788bc78be3b6e4
Anna Zayarova <annazayarova@Annas-MacBook-Pro.local>
2019-03-15 17:18:07 +0300

    Updates

---
+++ precache-manifest.f9f078d7e7bba7b303a6413a8baf867d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9d99fb5db03f9abb0db5",
+    "url": "/static/js/main.25091093.chunk.js"
+  },
+  {
+    "revision": "d10b4e4f5dfbb08540a3",
+    "url": "/static/js/2.e4f301ef.chunk.js"
+  },
+  {
+    "revision": "562b33f27c5855566e382ab3d736aed9",
+    "url": "/index.html"
+  }
+];
